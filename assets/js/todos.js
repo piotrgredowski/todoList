79871902541f02dcbe1f6c413228908552c086b8
Piotr Gredowski <piotrgredowski@gmail.com>
2017-05-24 14:27:45 +0200

    Added function of editing todos. User can apply changes by clicking on check icon or by using enter when typing in input.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,23 @@
 // check off specific todos by clicking
+// var liStructure = [
+//   "<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> ",
+//   "<span><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></span></li>"
+// ];
 var liStructure = [
-  "<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> ",
-  "<span><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></span></li>"
+  "<li><div class=\"deleteButton\"><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></div><div class=\"content\">",
+  "</div><div class=\"editButton\"><i class=\"fa fa-pencil-square-o\" aria-hidden=\"true\"></i></div></li>"
 ];
 
 var inputStructure = [
-  "<input class=\"edit\" type=\"text\"></input>"
+  "<div><input class=\"editInput\" type=\"text\"></input></div><div class=\"confirm\"><i class=\"fa fa-check\" aria-hidden=\"true\"></i></div>"
 ];
 
-$("ul").on("click", "li", function () {
+$("ul").on("click", ".content", function () {
   $(this).toggleClass("completed");
 });
 
 // click on X to delete todo
-$("ul").on("click", "span:first-of-type", function (event){
+$("ul").on("click", ".delete", function (event){
   $(this).parent().fadeOut(500, function () {
     // these 'this's are not the same - that from up is to span, that down is for its parent, li
     $(this).remove();
@@ -22,18 +26,39 @@ $("ul").on("click", "span:first-of-type", function (event){
 });
 
 // click on edit icon to edit
-$("ul").on("click", "span:nth-of-type(2)", function (event){
+$("ul").on("click", ".editButton", function (event){
   var li = $(this).parent();
   var textFromLi = li.text();
   textFromLi = textFromLi.trim();
   // var testLi = $('#test');
+  var parent = $(this).parent();
 
 
   li.html(inputStructure[0]);
   var inputToEdit = li.find("input");
   inputToEdit.attr("value", textFromLi);
   inputToEdit.focus();
-  console.log(inputToEdit);
+
+  function saveChangedTodo() {
+    parent.html(liStructure[0] + inputToEdit.val() + liStructure[1]);
+    parent.off();
+    console.log(1);
+  }
+
+  $(parent.find('.confirm')).on("click", function () {
+    saveChangedTodo();
+    // console.log(inputToEdit.val());
+  });
+  $(parent.find("input[type='text']")).keypress(function (event) {
+    if (event.which === 13) {
+      saveChangedTodo();
+    }
+  })
+
+  // console.log(parent.find('.confirm'));
+  // console.log(inputToEdit.val());
+
+
 
   event.stopPropagation();
 });
